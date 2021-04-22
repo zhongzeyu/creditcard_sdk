886c3e4de0ae87beaea428f918dd183ffcc33069
unknown <zhongzeyu@hotmail.com>
2021-04-22 13:08:36 -0700

    new

---
+++ IOTPayAndroidCredit/src/main/java/iotpay/androidcredit/widgets/IOTPayCard.java
@@ -0,0 +1,33 @@
+package iotpay.androidcredit.widgets;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import java.util.List;
+import iotpay.androidcredit.util.IOTPayEditText;
+
+public class IOTPayCard extends RelativeLayout {
+    private static List<IOTPayEditText> iotPayEditTextList;
+    public IOTPayCard(Context context) {
+        super(context);
+    }
+
+    public IOTPayCard(Context context, AttributeSet attrs) {
+        super(context);
+    }
+
+    public IOTPayCard(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public IOTPayCard(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context);
+    }
+
+
+}
